2420a92b615d3e73bdd116fcf03e2c662ffd57ce
andre azu <andreazuaa@gmail.com>
2022-02-15 09:56:20 +0300

    manipulate the html content using (.innerHTML)

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("hello!")
+document.getElementById("button").onclick=function(){
+    // .innerHTML Changes the content of the page 
+    document.getElementById("confirm").innerHTML="Order placed await email confirmation";
+
+    // Make the button dissapear
+    document.getElementById("button").style.display='none';
+}
