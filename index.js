c2f0da5caf33a94da6d4c894ebe18113ec6a9b07
andre azu <andreazuaa@gmail.com>
2022-02-15 10:22:36 +0300

    combine the previous two commits

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-document.getElementById("button").onclick=function(){
-    // .innerHTML Changes the content of the page 
-    document.getElementById("confirm").innerHTML="Order placed await email confirmation";
-
-    // Make the button dissapear
-    document.getElementById("button").style.display='none';
-}
-
 var username;
 username=prompt();
 message = "Your name is " + username
 console.log(message)
+
+document.getElementById("button").onclick=function(){
+    // .innerHTML Changes the content of the page 
+    document.getElementById("confirm").innerHTML=message;
+
+    // Make the button dissapear
+    document.getElementById("button").style.display='none';
+}
